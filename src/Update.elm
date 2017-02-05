3faf748eddaf20f714a7bc8016f66bed45832dcf
Franck Verrot <franck@verrot.fr>
2017-02-05 10:30:55 -0800

    Started refactoring rules

--- src/Update.elm
+++ src/Update.elm
@@ -20,19 +20,22 @@ type alias GameStatus = Result String Player
 playerWon : Player -> Model -> GameStatus
 playerWon player model =
   let
-      myGet i =  Maybe.withDefault Unclaimed (get i model.boxes)
-      isPlayer x = x == player
-      check a b c = (3 == length(filter isPlayer (fromList [(myGet a), (myGet b), (myGet c)])))
-      row1  = check 0 1 2
-      row2  = check 3 4 5
-      row3  = check 6 7 8
+      check : List Int -> Array Player -> Bool
+      check indices ary = fromList indices
+                            |> map (\index -> (get index ary |> Maybe.withDefault Unclaimed))
+                            |> filter ((==) player)
+                            |> length
+                            |> (==) 3
+      row1  = check [0, 1, 2] model.boxes
+      row2  = check [3, 4, 5] model.boxes
+      row3  = check [6, 7, 8] model.boxes
 
-      col1  = check 0 3 6
-      col2  = check 1 4 7
-      col3  = check 2 5 8
+      col1  = check [0, 3, 6] model.boxes
+      col2  = check [1, 4, 7] model.boxes
+      col3  = check [2, 5, 8] model.boxes
 
-      diag1 = check 0 4 8
-      diag2 = check 6 4 2
+      diag1 = check [0, 4, 8] model.boxes
+      diag2 = check [6, 4, 2] model.boxes
   in
      if row1 || row2 || row3 || col1 || col2 || col3 || diag1 || diag2 then
        Ok model.currentPlayer
